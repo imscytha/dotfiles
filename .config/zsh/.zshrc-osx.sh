7af5e875769a267d6aabc140af7c97590deb6836
Scytha <imscytha@protonmail.com>
2025-08-05 14:01:58 +0200

    feat(zsh): alias diff to git diff to have colors in macos

--- .config/zsh/.zshrc-osx.sh
+++ .config/zsh/.zshrc-osx.sh
@@ -7,3 +7,15 @@ fi
 alias ls='ls -G'
 alias work='cd $HOME/Developer/Spaxel/Repos/'
 alias scytha='cd $HOME/Developer/Scytha/'
+
+# Considering MacOS utilities are based on BSD utils[1] they don't support
+# colors natively[2] like GNU coreutils (with `--color` flag)[3]. Instead of
+# installing extra dependencies for adding colors on top of diff, utilize git's
+# diff engine that supports colors[4]
+#
+# ref:
+# [1]: https://man.freebsd.org/cgi/man.cgi
+# [2]: https://ss64.com/mac/diff.html
+# [3]: https://man7.org/linux/man-pages/man1/diff.1.html
+# [4]: https://git-scm.com/docs/git-diff
+alias diff='git diff --no-index --color=always'
