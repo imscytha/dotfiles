48c5be1b4cee6ecc7aca88f152a0922a0460c13f
Scytha <imscytha@protonmail.com>
2026-01-01 18:46:47 +0100

    WIP feat(nvim): add `uga-rosa/ccc.nvim`

---
+++ .config/nvim/lua/plugins/ccc.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "uga-rosa/ccc.nvim",
+    opts = {
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+      win_opts = {
+        border = "rounded",
+      },
+    },
+  },
+}
